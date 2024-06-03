c1db692f114fb43e58e86551b2946e82cc483df1
Makoto Yamashita <makochan12.9@gmail.com>
2024-06-04 01:42:20 +0900

    待機画面fin

--- graphics/js/break.js
+++ graphics/js/break.js
@@ -1,8 +1,46 @@
 const scheduleBoxElement = document.getElementById("schedule-box");
 
+const generateSchedule = () => {
+    const generateScheduleTime = (num, soon = false) => {
+        const data = presentationData[num];
+
+        const div = document.createElement("div");
+        div.classList.add("schedule-time");
+        if (soon) div.innerText = "もうすぐ";
+        else div.innerText = data.time + " -";
+        return div;
+    }
+
+    scheduleBoxElement.innerHTML = "";
+    const dataItemNum = Number(scheduleBoxElement.getAttribute("data-item-num"));
+
+    for (i = speakerNum; i < speakerNum + dataItemNum; i++) {
+        if (i >= presentationData.length) break;   // データがない場合は生成しない(スケジュールがない場合
+        const scheduleItemElement = document.createElement("div");
+        scheduleItemElement.classList.add("schedule-item");
+        scheduleItemElement.appendChild(generateScheduleTime(i, (i == speakerNum)));
+
+        const programElement = document.createElement("div");
+        programElement.classList.add("program");
+
+        const titleElement = document.createElement("div");
+        titleElement.classList.add("title");
+        titleElement.innerText = presentationData[i].title;
+        programElement.appendChild(titleElement);
+        const speakerElement = document.createElement("div");
+        speakerElement.classList.add("speaker");
+        speakerElement.innerText = presentationData[i].name;
+        programElement.appendChild(speakerElement);
+
+        scheduleItemElement.appendChild(programElement);
+        scheduleBoxElement.appendChild(scheduleItemElement);
+    }
+}
+
 const updateData = () => {
     loadData().then(() => {
         if (/*!presSpeakerInfoElement.hasChildNodes() || */!infoBarTextBoxElement.hasChildNodes()) {   // 初回のみ。動的に動くリストに何も生成されていない場合。同じ発表者で情報更新の時はアニメーション切り替えの時に反映される。発表者交代の時は下の関数で反映される。
+            generateSchedule();
             initNotice();
         }
     });
@@ -13,5 +51,6 @@ nodecg.listenFor("reloadData", updateData);
 
 nodecg.Replicant("breakNum").on("change", (newValue, oldValue) => {
     speakerNum = newValue;
+    generateSchedule();
     initNotice();
 });
